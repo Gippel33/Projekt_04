10c3d6ca95f59823ee2905a304546e26abb59829
Gippel33 <gippel@seznam.cz>
2025-07-29 20:26:46 +0200

    Upraveno po zhodnocení do finální podoby

--- main.py
+++ main.py
@@ -1,64 +1,89 @@
-# projekt_4.py: čtvrtý projekt do Engeto Online Python Akademie
+# projekt_04.py: čtvrtý projekt do Engeto Online Python Akademie
 
 # author: Pavel Nováček
 # email: gippel@seznam.cz
 
 
 def hlavni_menu():
+    """
+    Zobrazí hlavní menu a umožní spustit funkci dle volby 1-4.
+    """
     while True:
-        print("Správce úkolů - Hlavní menu",
-              "1. Přidat nový úkol",
+        print("Správce úkolů - hlavní menu",
+              "1. Přidat úkol",
               "2. Zobrazit všechny úkoly",
-              "3. Odstranit úkol",
+              "3. Vymazat úkol",
               "4. Konec programu",
               sep="\n"
               )
         try:
-            vstup = int(input("Vyberte možnost (1-4): "))
+            vstup = int(input("Zvolte 1-4: "))
             if vstup not in (1, 2, 3, 4):
-                print("Volba mimo rozsah. Zadejte číslo 1-4.\n")
+                print("Volba mimo rozsah. Napište číslo 1-4.\n")
                 continue
             return vstup
         except ValueError:
-            print("Neplatná volba.\n")
+            print("Neplatný vstup. Napište číslo 1-4.\n")
             continue
 
 
-def pridat_ukol():
+def pridat_ukol_input():
+    """
+    Umožní přidat úkol ve formátu: název a popis.
+
+    Příklad:
+    Zadejte název úkolu: Test1
+    Zadejte popis úkolu: Toto je test.
+    """
     while True:
         print("Pro návrat do hlavního menu napište 'z'")
         nazev = input("Zadejte název úkolu: ")
         if nazev == "":
-            print("Zadali jste prázdný vstup.\n")
+            print("Zadali jste prázdný vstup. Zadejte název úkolu.\n")
             continue
         elif nazev == "z":
             print()
-            break
+            return None
         popis = input("Zadejte popis úkolu: ")
         if popis == "":
-            print("Zadali jste prázdný vstup.\n")
+            print("Zadali jste prázdný vstup. Zadejte popis úkolu.\n")
             continue
         elif popis == "z":
             print()
-            break
+            return None
+        return nazev, popis
+
+
+def pridat_ukol(nazev, popis):
+    if nazev and popis:
         print(f"Úkol {nazev} byl přidán.")
         return {nazev: popis}
 
 
 def zobrazit_ukoly(seznam):
+    """
+    Zobrazí uložené úkoly.
+
+    Příklad:
+    1. Test1 - Toto je test.
+    """
     if len(seznam) == 0:
-        print("Seznam úkolů je prázdný.",
-              "\n", end=""
-              )
+        print("Seznam úkolů je prázdný.\n")
+        return True
     else:
         for pozice_seznamu, ukol in enumerate(seznam, start=1):
             for klic, hodnota in ukol.items():
                 print(f"{pozice_seznamu}. {klic} - {hodnota}")
     print()
+    return False
 
 
 def odstranit_ukol(seznam):
-    zobrazit_ukoly(seznam)
+    """
+    Umožní odstranit úkol ze seznamu.
+    """
+    if zobrazit_ukoly(seznam):
+        return
 
     while True:
         print("Pro návrat do hlavního menu napište 'z'")
@@ -77,18 +102,21 @@ def odstranit_ukol(seznam):
                 print(f"Úkol {vstup} byl vymazán.\n")
                 break
         except ValueError:
-            print("Zadali jste prázdný vstup.\n")
+            print("Zadali jste prázdný vstup. Zadejte číslo.\n")
             continue
 
 
 def main():
+    """
+    Hlavní běh programu, který spouští funkci dle volby.
+    """
     while True:
         volba = hlavni_menu()
         print()
         if volba == 1:
-            ukol = pridat_ukol()
+            ukol = pridat_ukol_input()
             if ukol:
-                ukoly.append(ukol)
+                ukoly.append(pridat_ukol(ukol[0], ukol[1]))
                 print()
         elif volba == 2:
             zobrazit_ukoly(ukoly)
